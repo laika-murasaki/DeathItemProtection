0c6bf7a75e07c216124ec1a67734d43dc2cecc26
Laika <111684641+laika-murasaki@users.noreply.github.com>
2023-01-13 04:23:16 +0700

    what location for enitiy ?

--- src/main/java/deathitemprotection/deathitemprotection/DeathItemProtectionEvents.kt
+++ src/main/java/deathitemprotection/deathitemprotection/DeathItemProtectionEvents.kt
@@ -1,5 +1,8 @@
 package deathitemprotection.deathitemprotection
 
+import com.comphenix.protocol.PacketType
+import com.comphenix.protocol.ProtocolLibrary
+import com.comphenix.protocol.wrappers.BlockPosition
 import org.bukkit.Bukkit
 import org.bukkit.NamespacedKey
 import org.bukkit.entity.Player
@@ -10,11 +13,15 @@ import org.bukkit.event.entity.ItemMergeEvent
 import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.persistence.PersistentDataType
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 class DeathItemProtectionEvents : Listener {
     companion object {
         private val deathKey = NamespacedKey(Bukkit.getPluginManager().getPlugin("deathitemprotection")!!, "death_items")
+        private val locationKey = NamespacedKey(Bukkit.getPluginManager().getPlugin("deathitemprotection")!!, "location")
+        private val protocolManager = ProtocolLibrary.getProtocolManager()
     }
+
 
     @EventHandler
     fun onPlayerDeath(event: PlayerDeathEvent) {
@@ -24,6 +31,16 @@ class DeathItemProtectionEvents : Listener {
             val dataContainer = meta.persistentDataContainer
             dataContainer.set(deathKey, PersistentDataType.STRING, player.uniqueId.toString())
             item.itemMeta = meta
+
+            // Add hover text to show the timer and the owner's name
+            val packet = protocolManager.createPacket(PacketType.Play.Server.WORLD_EVENT)
+            packet.integers.write(0, 2001) // 2001 is the id of the "create marker" event
+            val position = BlockPosition(item.location.x, item.location.y, item.location.z)
+            packet.blockPositionModifier.write(0, position)
+            packet.integers.write(1, 0) // Color of the marker, encoded as an ARGB integer (0 = white)
+            packet.strings.write(0, "Item Owned by ${player.name}") // Hover text
+            packet.integers.write(2, TimeUnit.SECONDS.toMillis(30).toInt()) // Lifetime of the marker in milliseconds
+            protocolManager.sendServerPacket(player, packet)
         }
     }
 
